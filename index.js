e44a8a3f5b519f28880035f5a2952246c3a89f9e
Wesley Chen <weschen1996@gmail.com>
2020-03-09 18:34:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(array){
+  let ans = array.find( obj => obj.result === "W" )
+  return !!ans ? ans.year : undefined
 }
