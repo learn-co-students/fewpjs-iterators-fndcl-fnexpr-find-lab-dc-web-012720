4f1a9631887533fa419d513b2a175cda506ae46f
Alan Banks <alanbanks229@gmail.com>
2020-03-09 19:21:29 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(arr_of_objects){
+  for (let i = 0; i < arr_of_objects.length; i++) {
+    if (arr_of_objects[i]["result"] === "W") {
+      return arr_of_objects[i]["year"]
+    }
+  }
+  return undefined
+}
