1fea3cd8e125c3f10edeca934392daa275df53d1
willmharris <willmharris0@gmail.com>
2020-03-09 21:40:02 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]["result"] === "W") {
+      return array[i]["year"]
+    }
+  }
+}
